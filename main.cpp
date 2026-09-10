56174445081db312595b9839df94d866d0017bf3
andresaugom <gomez.moreno.andres.21@gmail.com>
2026-09-10 17:39:58 -0600

    feature: see below \ - Added longest_palindrome function \ - Added 2 hardcoded tests to show functionality \ - Added the return 0 ; statement, correctness

--- main.cpp
+++ main.cpp
@@ -75,6 +75,34 @@ int kmp(string transmission, string mcode){
     return -1;
 }
 
+int longest_palindrome(string s) {
+    int n = s.length();
+    if (n == 0) return 0;
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int maxLength = 1;
+
+    for (int i = 0; i < n; i++) {
+	dp[i][i] = true;
+    }
+
+    for (int length = 2; length <= n; length++) {
+	for (int i = 0; i < n - length + 1; i++) {
+	    int j = i + length - 1;
+	    if (s[i] == s[j]) {
+		if (length == 2) {
+		    dp[i][j] = true;
+		} else {
+		    dp[i][j] = dp[i + 1][j - 1];
+		}
+		if (dp[i][j]) {
+		    maxLength = max(maxLength, length);
+		}
+	    }
+	}
+    }
+    return maxLength;
+}
+
 int main(){
     string transmission1 = leerArchivo("transmission1.txt");
     string transmission2 = leerArchivo("transmission2.txt");
@@ -93,4 +121,11 @@ int main(){
     else {
         cout << "false" << endl;
     }
+
+    string prueba1 = "aabbaa";
+    string prueba2 = "abcde";
+    cout << "Longest Palindrome in '" << prueba1 << "' is of length: " << longest_palindrome(prueba1) << endl;
+    cout << "Longest Palindrome in '" << prueba2 << "' is of length: " << longest_palindrome(prueba2) << endl;
+
+    return 0;
 }
