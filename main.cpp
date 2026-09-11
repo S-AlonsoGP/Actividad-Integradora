a475159a3d9ce7efd368b8a1c2aa71872fbb64fc
andresaugom <gomez.moreno.andres.21@gmail.com>
2026-09-10 18:17:35 -0600

    fix: now we track start and end index for palindrome discovery

--- main.cpp
+++ main.cpp
@@ -75,33 +75,37 @@ int kmp(string transmission, string mcode){
     return -1;
 }
 
-int longest_palindrome(string s) {
-    int n = s.length();
-    if (n == 0) return 0;
-    vector<vector<bool>> dp(n, vector<bool>(n, false));
-    int maxLength = 1;
-
-    for (int i = 0; i < n; i++) {
-	dp[i][i] = true;
+// helper function
+int expand_around_center(string *s, int left, int right) {
+    while (left >= 0 && right < s->length() && (*s)[left] == (*s)[right]) {
+	left--;
+	right++;
     }
+    return right - left - 1;
+}
 
-    for (int length = 2; length <= n; length++) {
-	for (int i = 0; i < n - length + 1; i++) {
-	    int j = i + length - 1;
-	    if (s[i] == s[j]) {
-		if (length == 2) {
-		    dp[i][j] = true;
-		} else {
-		    dp[i][j] = dp[i + 1][j - 1];
-		}
-		if (dp[i][j]) {
-		    maxLength = max(maxLength, length);
-		}
-	    }
+// this function returns the starting and ending indices of the longest palindromic substring in the given string s
+// using *s for large strings to avoid copying
+pair<int, int> longest_palindrome (string *s) {
+	
+    int n = s->length();
+    if (n == 0) {
+	return {-1, -1};
+    }
+    int start = 0;
+    int end = 0;
+    for (int i = 0; i < n; i++) {
+	int len1 = expand_around_center(s, i, i);
+	int len2 = expand_around_center(s, i, i + 1);
+	int len = max(len1, len2);
+	if (len > end - start) {
+	    start = i - (len - 1) / 2;
+	    end = i + len / 2;
 	}
     }
-    return maxLength;
+    return {start, end};
 }
+
 
 int main(){
     string transmission1 = leerArchivo("transmission1.txt");
@@ -122,10 +126,25 @@ int main(){
         cout << "false" << endl;
     }
 
-    string prueba1 = "aabbaa";
-    string prueba2 = "abcde";
-    cout << "Longest Palindrome in '" << prueba1 << "' is of length: " << longest_palindrome(prueba1) << endl;
-    cout << "Longest Palindrome in '" << prueba2 << "' is of length: " << longest_palindrome(prueba2) << endl;
+/**
+ * Assuming that malicious code always has "mirrored" code (chars palindromes), it would be a good idea to look for this type of code in a transmission. The program should then look for "mirrored" code within the transmission files (only at chars level, do not look for it at bits level). The program displays in a single line (two integers separated by a space) the position (starting at 1) where the longest "mirrored" code (palindrome) for each stream file starts and ends. It can be assumed that this type of code will always be found.
+ */
+
+    pair<int, int> longest1 = longest_palindrome(&transmission1);
+    pair<int, int> longest2 = longest_palindrome(&transmission2);
+
+    if (longest1.first != -1 && longest1.second != -1) {
+	cout << "Longest palindrome in transmission1: " << longest1.first + 1 << " " << longest1.second + 1 << endl;
+    } else {
+	cout << "No palindrome found in transmission1" << endl;
+    }
+
+    if (longest2.first != -1 && longest2.second != -1) {
+	cout << "Longest palindrome in transmission2: " << longest2.first + 1 << " " << longest2.second + 1 << endl;
+    } else {
+	cout << "No palindrome found in transmission2" << endl;
+    }
 
     return 0;
 }
+
